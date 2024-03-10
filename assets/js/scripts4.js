7c7e923600b66c141a241af9f2ca115561ec611b
Jorge Solis <jorge.solis.sm@gmail.com>
2024-03-10 18:41:54 -0300

    se arreglan detalles en el codigo scripts4

--- assets/js/scripts4.js
+++ assets/js/scripts4.js
@@ -4,6 +4,12 @@ let nume3 = prompt("Ingresar tercer número");
 let nume4 = prompt("Ingresar cuarto número");
 let nume5 = prompt("Ingresar quinto número");
 
+nume1 = Number(nume1);
+nume2 = Number(nume2);
+nume3 = Number(nume3);
+nume4 = Number(nume4);
+nume5 = Number(nume5);
+
 let promedio = (nume1 + nume2 + nume3 + nume4 + nume5) / 5;
 
 document.writeln("<p>El resultado del promedio es: " + promedio + "</p>");
