69b02f140fe2a4d405ab94a75006dfb6d337c7dc
Jorge Solis <jorge.solis.sm@gmail.com>
2024-03-10 15:14:20 -0300

    se busca y se realiza calculo

--- assets/js/scripts3.js
+++ assets/js/scripts3.js
@@ -1,20 +1,32 @@
-let diastotales = 373;
-let anios = 0;
-let semanas = 0;
-let dias = 0;
+// let diastotales = 373;
+// let anios = 0;
+// let semanas = 0;
+// let dias = 0;
 
-if(diastotales >= 365){
-    anios = Math.floor(diastotales/365)
-    diastotales = diastotales - (anios * 365);
-}
-console.log("Dias Totales", diastotales);
-if(diastotales >= 7){
-    semanas = Math.floor(diastotales/7);
-    diastotales = diastotales - (semanas * 7);
-}
-dias = diastotales;
+// if(diastotales >= 365){
+//     anios = Math.floor(diastotales/365)
+//     diastotales = diastotales - (anios * 365);
+// }
+// console.log("Dias Totales", diastotales);
+// if(diastotales >= 7){
+//     semanas = Math.floor(diastotales/7);
+//     diastotales = diastotales - (semanas * 7);
+// }
+// dias = diastotales;
 
-console.log ("años", anios);
-console.log ("Semanas", semanas);
-console.log("Dias", dias);
+// console.log ("años", anios);
+// console.log ("Semanas", semanas);
+// console.log("Dias", dias);
 // ver si se puede realizar de otra forma
+
+
+let cifrasdias = prompt("Ingresar cifra");
+
+let an = Math.floor(cifrasdias/365);
+let sema = Math.floor((cifrasdias % 365) / 7);
+let dias = Math.floor((cifrasdias % 7) - 1);
+
+
+let resultado = "Años: " + an + ", Semanas" + sema + ", Días" + dias;
+
+document.writeln(resultado);
