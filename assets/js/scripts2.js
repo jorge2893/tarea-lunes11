5295fde0c87ea86f0e5365013f79b5e949fb85cc
Jorge Solis <jorge.solis.sm@gmail.com>
2024-03-09 23:28:05 -0300

    se hace proyecto de temperaturas

---
+++ assets/js/scripts2.js
@@ -0,0 +1,9 @@
+let numero1 = prompt("Ingrese numero/temperatura");
+
+let kelvin = numero1 + 273.15;
+
+document.writeln("La temperatura en Grados Kelvin es: ", + kelvin);
+
+let Fahrenheit = (numero1 * 9/5) + 32;
+
+document.writeln("La temperatura en Fahrenheit es: ", + Fahrenheit);
