c3026fa547bbd87687ccc902568c5d3eccbedd75
Jorge Solis <jorge.solis.sm@gmail.com>
2024-03-10 18:37:14 -0300

    se agrega ficha promedio

---
+++ assets/js/scripts4.js
@@ -0,0 +1,9 @@
+let nume1 = prompt("Ingresar primer número");
+let nume2 = prompt("Ingresar segundo número");
+let nume3 = prompt("Ingresar tercer número");
+let nume4 = prompt("Ingresar cuarto número");
+let nume5 = prompt("Ingresar quinto número");
+
+let promedio = (nume1 + nume2 + nume3 + nume4 + nume5) / 5;
+
+document.writeln("<p>El resultado del promedio es: " + promedio + "</p>");
