fa4f7c11c3f5a5d365c47ccf396ad026f1ce0c93
Jorge Solis <jorge.solis.sm@gmail.com>
2024-03-09 23:32:30 -0300

    se arregla Number

--- assets/js/scripts2.js
+++ assets/js/scripts2.js
@@ -1,9 +1,9 @@
-let numero1 = prompt("Ingrese numero/temperatura");
+let numero1 = Number(prompt("Ingrese numero/temperatura"));
 
 let kelvin = numero1 + 273.15;
 
-document.writeln("La temperatura en Grados Kelvin es: ", + kelvin);
+document.writeln("<p> La temperatura en Grados Kelvin es: " + kelvin + "</p>");
 
 let Fahrenheit = (numero1 * 9/5) + 32;
 
-document.writeln("La temperatura en Fahrenheit es: ", + Fahrenheit);
+document.writeln("<p> La temperatura en Fahrenheit es: " + Fahrenheit + "</p>");
